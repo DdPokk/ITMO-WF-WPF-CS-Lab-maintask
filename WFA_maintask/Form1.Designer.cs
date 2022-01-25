be708d4d71c159b860e806477d62fe10ca0833b2
Dmitry Yudin <DmitryYudin@itcenterifmo.onmicrosoft.com>
2022-01-25 19:22:37 +0300

    1 part

--- WFA_maintask/Form1.Designer.cs
+++ WFA_maintask/Form1.Designer.cs
@@ -28,13 +28,45 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmdTest = new System.Windows.Forms.Button();
+            this.listBoxResult = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmdTest
+            // 
+            this.cmdTest.Location = new System.Drawing.Point(51, 12);
+            this.cmdTest.Name = "cmdTest";
+            this.cmdTest.Size = new System.Drawing.Size(430, 39);
+            this.cmdTest.TabIndex = 1;
+            this.cmdTest.Text = "Test";
+            this.cmdTest.UseVisualStyleBackColor = true;
+            this.cmdTest.Click += new System.EventHandler(this.cmdTest_Click);
+            // 
+            // listBoxResult
+            // 
+            this.listBoxResult.FormattingEnabled = true;
+            this.listBoxResult.Location = new System.Drawing.Point(-1, 78);
+            this.listBoxResult.Name = "listBoxResult";
+            this.listBoxResult.Size = new System.Drawing.Size(555, 277);
+            this.listBoxResult.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(548, 346);
+            this.Controls.Add(this.listBoxResult);
+            this.Controls.Add(this.cmdTest);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button cmdTest;
+        private System.Windows.Forms.ListBox listBoxResult;
     }
 }
 
--- WFA_maintask/Form1.cs
+++ WFA_maintask/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,28 @@ namespace WFA_maintask
         public Form1()
         {
             InitializeComponent();
+        }
+        private void listBoxResult_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmdTest_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ApressFinancial;Data Source=.\\SQLExpress"))
+            {
+                cn.Open();
+                using (SqlCommand cmd = new SqlCommand("SELECT ShareDesc, CurrentPrice FROM ShareDetails.Shares", cn))
+                {
+                    using (SqlDataReader read = cmd.ExecuteReader())
+                    {
+                        while (read.Read())
+                        {
+                            listBoxResult.Items.Add(read[0] + "  " + read[1]);
+                        }
+                    }
+                }
+            }
         }
     }
 }
